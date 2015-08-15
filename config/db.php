46a11a75f84029aa43a31f96d6a03d8178f175b4
andrey3 <rus_andrey3@mail.ru>
2015-08-15 23:44:39 +0300

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2shop',
-    'username' => 'root',
-    'password' => '1234',
-    'charset' => 'utf8',
-];
